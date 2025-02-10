76171b67c20523e833c58df57c6736fe60ba7c09
nhnghia <nhnghia@localhost>
2025-02-10 10:51:32 +0100

    add test results

--- linuxptp/tc-p4/test/result/topos-3/plot-all.py
+++ linuxptp/tc-p4/test/result/topos-3/plot-all.py
@@ -27,7 +27,7 @@ def parse_ptp_log(log_file):
     # test_string = 'ptp4l[3235037.837]: rms 324564 max 326176 freq -8602326 +/-  77 delay 334763 +/-   0'
     
     data = {
-        #"timestamp": [],
+        "timestamp": [],
         "master_offset": [],
         "frequency_offset": [],
         "path_delay": []
@@ -39,8 +39,8 @@ def parse_ptp_log(log_file):
         re.IGNORECASE
     )
     
-    min_ts   =  0
-    interval = 300
+    min_ts   =  20
+    interval = 320
     
     first_time = 0
     with open(log_file, "r") as file:
@@ -66,10 +66,10 @@ def parse_ptp_log(log_file):
                 if kernel_time > interval:
                     break
          
-                #data["timestamp"].append(int(kernel_time))
-                data["master_offset"].append(abs(float(master_offset))/1000)
+                data["timestamp"].append(int(kernel_time))
+                data["master_offset"].append(float(master_offset))
                 data["frequency_offset"].append(float(freq))
-                data["path_delay"].append(float(path_delay)/1000)
+                data["path_delay"].append(float(path_delay))
     
     return data
 
@@ -89,88 +89,28 @@ def annotate_boxplot(ax, data):
                 f"Median: {stats['median']:.2f}\n"
                 f"Q1: {stats['q1']:.2f}\n"
                 f"Q3: {stats['q3']:.2f}"], loc="upper right", fontsize=10)
-
-def moving_average(y_2d, window_size=3):
-    #homogenous row size
-    min_len = 1000
-    for arr in y_2d:
-        if min_len > len(arr):
-            min_len = len(arr)
-
-    new_y_2d = []
-    for arr in y_2d:
-        new_y_2d.append( arr[:min_len])
-
-    #get a average row
-    mean = np.mean(new_y_2d, axis=0)
-
-    #smooth the line
-    return np.convolve(mean, np.ones(window_size)/window_size, mode='valid')
-
-y_labels = {
-        "master_offset"   : "Clock offset (microsecond)", 
-        "frequency_offset": "Frequency offset", 
-        "path_delay"      : "Path delay (microsecond)"}
-
-def line_plot_metrics(data):
-    """
-    :param data: Dictionary containing timestamps and metric values.
-    """
-    #plt.figure(figsize=(15, 12))
-    #fig, ax = plt.subplots()
     
-    for name in data:
-        obj = data[name]
-        
-        for metric in obj:
-            plt.clf()
-        
-            #plt.yscale('log')  # Set Y-axis to logarithmic scale (base 10)
-
-            for arr in obj[metric]:
-                plt.plot(range(0, len(arr)), arr, marker='o', color="black", markeredgewidth=0, alpha=0.5, markersize=5, linestyle='None')
-            
-            # a line to represent average
-            y_smooth = moving_average(obj[metric], window_size=10)
-            plt.plot(range(0,len(y_smooth)), y_smooth, color="red")
-            
-            plt.xlabel("Timestamp (second)")
-            plt.ylabel( y_labels[metric] )
-    
-            plt.tight_layout()
-            plt.grid()
-            plt.savefig( f"plot-{name}-{metric}.pdf", dpi=30, format='pdf', bbox_inches='tight')
-
-def flatten( arr ):
-    ret = []
-    for val in arr:
-        ret += val
-    return ret
-
-def box_plot_metrics(data):
+def plot_metrics(data):
     """
     Plots the master offset, frequency offset, and path delay metrics along with boxplots.
     :param data: Dictionary containing timestamps and metric values.
     """
     #plt.figure(figsize=(15, 12))
     #fig, ax = plt.subplots()
-    for metric in y_labels:
+    metrics  = ["master_offset", "frequency_offset", "path_delay"]
+    y_labels = ["clock offset (ns)", "frequency offset", "path delay (ns)"]
+    for i in range(0, len(metrics)):
         plt.clf()
-        
-        #plt.yscale('log')  # Set Y-axis to logarithmic scale (base 10)
-
+        metric = metrics[i]
         labels = []
         arr    = []
-        # each log
-        for name in data:
-            labels.append( name )
-            val = data[name][metric]
-            val = flatten( val )
-            arr.append( val )
+        for label in data:
+            labels.append( label )
+            arr.append( data[label][metric] )
         # Boxplot for Master Offset (Vertical)
         box = plt.boxplot(arr, vert=True, patch_artist=True, labels=labels, boxprops=dict(color='black', facecolor='black', alpha=0.6), medianprops=dict(color='black'))
         #plt.title("Master Offset Distribution")
-        plt.ylabel( y_labels[metric] )
+        plt.ylabel( y_labels[i] )
 
         plt.tight_layout()
         plt.grid()
@@ -181,29 +121,16 @@ if __name__ == "__main__":
     extension = ".json.slave.log"
     # Set up command-line argument parsing
     log_files = ["1-switch", "2-switches", "5-switches", "10-switches", "20-switches"]
-    #log_files = ["1-switch"]
     print(log_files)
     data = dict()
-    
     for name in log_files:
-        data[name] = {}
-
-        #for i in range(0, 10):
-        for i in range(1, 11):
-            log_file = f"./topos-{i}/{name}{extension}"
-            print(f"parsing {log_file}")
-            # Parse the log file
-            obj = parse_ptp_log(log_file)
-            for metric in obj:
-                if metric not in data[name]:
-                    data[name][metric] = []
-                arr = obj[metric]
-                if len(arr):
-                    data[name][metric].append( arr )
+        log_file = f"./{name}{extension}"
+        print(f"parsing {name}")
+        # Parse the log file
+        data[name] = parse_ptp_log(log_file)
 
     # Plot the metrics
     if len(data):
-        #line_plot_metrics(data)
-        box_plot_metrics(data)
+        plot_metrics(data)
     else:
         print("No valid data found in the log file.")
